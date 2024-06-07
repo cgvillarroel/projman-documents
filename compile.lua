4b5fdba6f3c89b1824727f8bc2809048465f37c9
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2024-06-07 18:34:58 +0800

    chore: tweaks, better table semantics

--- compile.lua
+++ compile.lua
@@ -1,10 +1,10 @@
 -- I couldn't get registers to work in vim.cmd
 -- This function allows us to do what `<c-r>"` does
-_G.DumpContents = function()
-		vim.cmd('norm wwyt["_d$')
-		vim.cmd("read src/" .. vim.fn.getreg('"'))
-		vim.cmd("norm kdd")
-	end
+function _G.DumpContents()
+	vim.cmd('norm wwyt["_d$')
+	vim.cmd("read src/" .. vim.fn.getreg('"'))
+	vim.cmd("norm kdd")
+end
 
 vim.fn.delete("output/wiki.adoc")
 vim.cmd("e output/wiki.adoc")
